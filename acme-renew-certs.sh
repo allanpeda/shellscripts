a7267a2fe7787f602ff6ae0d53c156c9ac0b46b1
Allan Peda <allanpeda@users.noreply.github.com>
2026-04-18 22:21:58 -0400

    Update pem retrieval method in acme-renew-certs.sh
    Refactor certificate retrieval to use get_pem function.

--- acme-renew-certs.sh
+++ acme-renew-certs.sh
@@ -8,15 +8,21 @@ get_sites(){
     awk '"server" == $1 {sub(/^\"/,"",$2); sub(/\"$/,"",$2); print $2}' "$htconf"
 }
 
+get_pem(){
+    perl -ne 'BEGIN { $target = shift @ARGV }
+    if (/^domain\s+(\S+)/) { $in_block = ($1 eq $target); }
+    if ($in_block && /domain full chain certificate\s+"([^"]+)"/)
+      { print "$1\n"; exit }' "$1" "$2"
+}
+
 # sidestep subshell scope
 check_reload(){
     local site pem
     get_sites | while IFS= read -r site
     do
-        # testing the update modification time is easier to run repeatledly
-        pem="$('/usr/sbin/acme-client' -v -f \
-               '/etc/acme-client.conf' "$site" 2>&1 \
-                  | awk '{sub(/:/,"",$2); print $2}')"
+        pem="$(get_pem "$site" '/etc/acme-client.conf')"
+        # testing the update modification time is easier to do repeatledly
+        '/usr/sbin/acme-client' -f '/etc/acme-client.conf' "$site" &>/dev/null || :
         if (( $(date +%s) - $(stat -f %m "$pem") < 3600 ))
         then
             echo "1"
